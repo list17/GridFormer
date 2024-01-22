89b6cba35735178fb038c4d585f69134346b874c
list17 <1511500409@qq.com>
2024-01-22 12:57:57 +0800

    fix git ignore

---
+++ src/data/__init__.py
@@ -0,0 +1,31 @@
+
+from src.data.core import (
+    Shapes3dDataset, collate_remove_none, worker_init_fn
+)
+from src.data.fields import (
+    IndexField, PointsField, BoundaryPointsField,
+    VoxelsField, PatchPointsField, PointCloudField, PatchPointCloudField, PartialPointCloudField, 
+)
+from src.data.transforms import (
+    PointcloudNoise, SubsamplePointcloud,
+    SubsamplePoints,
+)
+__all__ = [
+    # Core
+    Shapes3dDataset,
+    collate_remove_none,
+    worker_init_fn,
+    # Fields
+    IndexField,
+    PointsField,
+    BoundaryPointsField,
+    VoxelsField,
+    PointCloudField,
+    PartialPointCloudField,
+    PatchPointCloudField,
+    PatchPointsField,
+    # Transforms
+    PointcloudNoise,
+    SubsamplePointcloud,
+    SubsamplePoints,
+]
